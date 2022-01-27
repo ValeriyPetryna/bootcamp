55869dbc7b29c15b9aead8648590d543e9b77aee
Valerii Petryna <valerii.petryna@intellias.com>
2022-01-27 14:21:18 +0200

    move db to cloud

--- api/utils/config.js
+++ api/utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: 3000,
   DIST: "dist/bootcamp/",
-  MONGO_URL: "mongodb://localhost:27017/blog",
+  MONGO_URL: "mongodb+srv://admin:admin@bci.egvlc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   SECRET: "secret-key"
 };
